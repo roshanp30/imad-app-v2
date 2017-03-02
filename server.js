1ea7c01e8f17a9a7819ff2a557f30daa66789068
roshanp30 <roshanpatil3004@gmail.com>
2017-03-03 00:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ function createTemplate (data){
                 </div>
                 <hr/>
                 <h3>${heading}</h3>
-                <div>${date}</div>
+                <div>${date.toDateString}</div>
                 <div>
                    ${content}
                 </div>
