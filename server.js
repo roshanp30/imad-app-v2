ee3a78fa378586b54c7b9aaa8b06876b24c33d6a
roshanp30 <roshanpatil3004@gmail.com>
2017-03-03 00:11:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,9 +92,9 @@ app.get('/test-db',function (req,res){
     });
     
 });
-app.get('/:articleName',function(req,res)
+app.get('/articles/:articleName',function(req,res)
 {
- var articleName = req.params.articleName;
+ //var articleName = req.params.articleName;
  pool.query("SELECT * FROM artice where title= '"+req.params.articeName+"'",function(err,result){
      if(err){
             res.status(500).send(err,toString());
