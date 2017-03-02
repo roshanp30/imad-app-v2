62b3441a3e3e0fa651e937c4024a7dd7bdf7bdf2
roshanp30 <roshanpatil3004@gmail.com>
2017-03-03 00:13:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/', function (req, res) {
 app.get('/test-db',function (req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
-            res.status(500).send(err,toString());
+            res.status(500).send(err.toString());
         }
         else{
             res.send(JSON.stringify(result));
@@ -97,7 +97,7 @@ app.get('/articles/:articleName',function(req,res)
  //var articleName = req.params.articleName;
  pool.query("SELECT * FROM artice where title= '"+req.params.articeName+"'",function(err,result){
      if(err){
-            res.status(500).send(err,toString());
+            res.status(500).send(err.toString());
         }
         else{
             if(result.row.length===0)
