1bbd6057e0a5cd43bf6fbc920ec5334d60caadc6
roshanp30 <roshanpatil3004@gmail.com>
2017-03-12 02:06:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var path = require('path');
 var app = express();
 var Pool =require('pg').Pool;
 var crypto=require(crypto);
+var bodyParser=require(body-parser);
 
 var config={
   user:'roshanp30',
@@ -15,6 +16,7 @@ var config={
   
 };
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data){
     var title=data.title;
@@ -50,6 +52,22 @@ function hash(input){
   var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
   return ['pbkdf2','10000',salt,hashed.toString('hex')].join('$');
 }
+
+
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomBytes(128).toString('hex');
+    var dbString=hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User successfully created'+username);
+        }
+    });
+});
 
 app.get('/hash/:input',function(req,res){
    var hashedString=hash(req.params.input,'this-is-some-random-string');
