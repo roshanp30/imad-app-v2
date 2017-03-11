2ba9733535c6544a6ab770c8dea70383af0d4c89
roshanp30 <roshanpatil3004@gmail.com>
2017-03-12 01:36:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 
 var app = express();
 var Pool =require('pg').Pool;
+var crypto=require(crypto);
 
 var config={
   user:'roshanp30',
@@ -45,7 +46,15 @@ function createTemplate (data){
     return htmlTemplate;
 }
 
+function hash(input){
+  var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+  return hashed.toString('hex');
+}
 
+app.get('/hash/:input',function(req,res){
+   var hashedString=hash(req.params.input,'this-is-some-string');
+   res.send(hashedString);
+});
 
 
 var pool = new Pool(config);
